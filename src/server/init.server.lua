f70633f0bcf8ceaa65531a82503020de882ed4c7
Great Bird <greatbabybird@gmail.com>
2023-05-01 20:02:52 -0700

    Add prototype logic and ignore sourcemap.json

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,16 @@
-print("Hello world, from server!")
+local RunService = game:GetService("RunService")
+
+-- while true do
+--     local gameIsOn = true
+--     start()
+--     replicateState()
+
+--     local connection
+--     connection = RunService.Heartbeat:Connect(function(deltaTime)
+--         if not gameIsOn then
+--             connection:Disconnect()
+--         end
+
+--         replicateState()
+--     end)
+-- end
