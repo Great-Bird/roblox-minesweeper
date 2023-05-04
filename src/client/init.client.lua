e6242dab5455b9cbc0745215ac9997a868f0ca12
Great Bird <greatbabybird@gmail.com>
2023-05-04 13:20:33 -0700

    Add BoardStateChanged handler

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,10 @@
-print("Hello world, from client!")
+--!strict
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Net = require(ReplicatedStorage.Shared.modules.Packages.net)
+
+local boardStateChanged = Net:RemoteEvent("BoardStateChanged")
+
+Net:Connect("BoardStateChanged", function(action)
+    print(`Received action`, action)
+end)
