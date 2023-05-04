19c609fdbd81bc502ce4ead9d22508767c3878a4
Great Bird <greatbabybird@gmail.com>
2023-05-04 13:21:59 -0700

    Remove unnecessary code

--- src/shared/components/Board.lua
+++ src/shared/components/Board.lua
@@ -15,29 +15,4 @@ export type Coordinates = {
     column: number,
 }
 
-local random = Random.new()
-
-
-local function Cell(isMine: boolean): Cell
-    return {
-        isCleared = false,
-        isFlagged = false,
-        isMine = isMine,
-    }
-end
-
-
-local Board = {}
-
-
--- TODO: benchmark to see whether or not shuffling makes iteration slower
-function Board.create(height: number, width: number, mines: number): Board
-    return {
-        height = height,
-        width = width,
-        cells = {},
-    }
-end
-
-
-return Board
+return nil
